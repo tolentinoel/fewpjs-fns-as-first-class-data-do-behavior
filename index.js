e5a80178ab21b5b56a91bc9884e1d4d1b92f091c
tolentinoel <rckrleyn@gmail.com>
2020-11-16 15:10:25 -0800

    Done.

--- index.js
+++ index.js
@@ -9,5 +9,22 @@ function handleClick(e) {
 
 /* End Given Code, don't edit above here...*/
 
+
 /* Write your implementation of greet() */
+function greet(str) {
+  let time = str.split(':');
+  let num = parseInt(time);
+  if (num < 12) {
+    return "Good Morning";
+  } else if (num < 17 || num == 12) {
+    return "Good Afternoon";
+  } else {
+    return "Good Evening";
+  }
+}
+
+
 /* Write your implementation of displayMessage() */
+function displayMessage(str) {
+  document.getElementById('greeting').innerText = str;
+}
